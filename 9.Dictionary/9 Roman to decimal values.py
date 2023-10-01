3d2dc4e3e5c1936f81104f6062f65ea6b30a7b00
arunprasadh7 <m.arunprasadh96@gmail.com>
2023-10-01 23:29:35 +0530

    Dictionary

--- 9.Dictionary/9 Roman to decimal values.py
+++ 9.Dictionary/9 Roman to decimal values.py
@@ -10,6 +10,7 @@ roman_values = {
     'M': 1000,
 }
 
+'''
 roman_numeral = "MCMXCIV"  # Example: 1994
 
 decimal = 0
@@ -26,3 +27,21 @@ for symbol in roman_numeral[::-1]:
     prev_value = value
 
 print(f"The decimal value of {roman_numeral} is {decimal}")
+'''
+
+#method 2
+
+number = input('Enter the roman number: ').upper()
+decimal = 0
+
+i = 0
+while i < len(number):
+    if (i+1) < len(number) and roman_values[number[i]] < roman_values[number[i+1]]:
+        decimal += roman_values[number[i+1]] - roman_values[number[i]]
+        i += 2
+    
+    else:
+        decimal += roman_values[number[i]]
+        i += 1
+
+print(f'The decimal value of \'{number}\' is {decimal}.')
